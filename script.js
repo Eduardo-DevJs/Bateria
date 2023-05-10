f251f11e629dd025222443796681affb1a5d5597
Eduardo de Oliveira Liria <dudev2001@gmail.com>
2023-05-10 12:04:12 -0300

    Bateria

---
+++ script.js
@@ -0,0 +1,40 @@
+document.body.addEventListener('keyup', (e) => {
+  playSound(e.code.toLocaleLowerCase());
+});
+
+document.querySelector('.composer button').addEventListener('click', () => {
+  let createSong = document.querySelector('#input').value;
+
+  if (createSong !== '') {
+    let songArray = createSong.split('');
+    playComposition(songArray);
+  }
+});
+
+function playSound(sound) {
+  let audioElement = document.querySelector(`#s_${sound}`);
+  let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+
+  if (audioElement) {
+    audioElement.currentTime = 0;
+    audioElement.play();
+  }
+
+  if (keyElement) {
+    keyElement.classList.add('active');
+
+    setTimeout(() => {
+      keyElement.classList.remove('active');
+    }, 300);
+  }
+}
+
+function playComposition(songArray) {
+  let wait = 0;
+  for (let songItem of songArray) {
+    setTimeout(() => {
+      playSound(`key${songItem}`);
+    }, wait);
+    wait += 250;
+  }
+}
